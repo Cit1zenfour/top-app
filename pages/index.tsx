b6a1c22183bc98df43a8774ccdfa388d523fbaf5
kamronshamsiev <kamron.shamsiev@hygge.software>
2021-12-08 20:47:18 +0500

    Initial Commit: created Next.JS project and added some simple components

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button, Htag } from "../components";
+
+export default function Home(): JSX.Element {
+  return (
+    <div>
+      <Htag tag="h1">Text</Htag>
+      <Button appearance="primary" arrow="right">
+        Кнопка
+      </Button>
+      <Button appearance="ghost">Кнопка</Button>
+    </div>
+  );
+}
